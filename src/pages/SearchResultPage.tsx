4d558382f7f50d7d33fb0b37803ef46064889a7f
min49590 <min49590@gmail.com>
2023-08-19 05:03:58 +0900

    Chore: 네이버 맵 API 호출

--- src/pages/SearchResultPage.tsx
+++ src/pages/SearchResultPage.tsx
@@ -1,8 +1,33 @@
 import { useParams, NavLink } from "react-router-dom";
 import { Button } from "@nextui-org/react";
+import { useEffect, useRef } from "react";
 
 export default function SearchResultPage() {
   const { keyword } = useParams();
+
+  const mapElement = useRef(null);
+
+  useEffect(() => {
+    const { naver } = window;
+    if (!mapElement.current || !naver) return;
+
+    // 지도에 표시할 위치의 위도와 경도 좌표를 파라미터로 넣어줍니다.
+    const location = new naver.maps.LatLng(37.5656, 126.9769);
+    const mapOptions: naver.maps.MapOptions = {
+      center: location,
+      zoom: 17,
+      zoomControl: true,
+      zoomControlOptions: {
+        position: naver.maps.Position.TOP_RIGHT,
+      },
+    };
+    const map = new naver.maps.Map(mapElement.current, mapOptions);
+    new naver.maps.Marker({
+      position: location,
+      map,
+    });
+  }, []);
+
   return (
     <>
       <h1>This is SearchResultPage.</h1>
@@ -10,6 +35,7 @@ export default function SearchResultPage() {
       <NavLink to="/">
         <Button>메인으로</Button>
       </NavLink>
+      <div ref={mapElement} style={{ minHeight: "400px" }} />
     </>
   );
 }
---
+++ src/utils/loadNaverMap.ts
@@ -0,0 +1,7 @@
+const script = document.createElement("script");
+script.type = "text/javascript";
+script.src = `https://openapi.map.naver.com/openapi/v3/maps.js?ncpClientId=${
+  import.meta.env.VITE_NAVER_MAP_API_KEY
+}`;
+document.body.appendChild(script);
+console.log(import.meta.env.VITE_NAVER_MAP_API_KEY);
