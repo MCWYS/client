937d54cc4da40df89914a11e9689cead4693a822
min49590 <min49590@gmail.com>
2023-08-18 20:02:35 +0900

    Init: nextui 설치 및 프로젝트 initialize

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,37 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import { NextUIProvider } from "@nextui-org/react";
+import { Button } from "@nextui-org/react";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <NextUIProvider>
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+        <h1>Hello world!</h1>
+        <Button color="primary">NextUI 버튼</Button>
+        <div className="flex flex-wrap gap-4 items-center">
+          <Button color="primary" variant="solid">
+            Solid
+          </Button>
+          <Button color="primary" variant="faded">
+            Faded
+          </Button>
+          <Button color="primary" variant="bordered">
+            Bordered
+          </Button>
+          <Button color="primary" variant="light">
+            Light
+          </Button>
+          <Button color="primary" variant="flat">
+            Flat
+          </Button>
+          <Button color="primary" variant="ghost">
+            Ghost
+          </Button>
+          <Button color="primary" variant="shadow">
+            Shadow
+          </Button>
+        </div>
+      </NextUIProvider>
     </>
   );
 }
