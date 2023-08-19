bba37d3518dfbdf11d7f4abf3e165ccad6541441
min49590 <min49590@gmail.com>
2023-08-20 06:07:01 +0900

    Style: favicon 넣음

--- src/pages/MyProfilePage.tsx
+++ src/pages/MyProfilePage.tsx
@@ -2,20 +2,16 @@ import { NavLink } from "react-router-dom";
 
 export function MyProfilePage() {
   return (
-    <>
-      <img src="/프로필 조회 화면.png" />
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
       <NavLink to="/myshoes">
-        <div
-          style={{
-            position: "fixed",
-            top: "40%",
-            left: "30%",
-            width: "40vw",
-            height: "30px",
-            zIndex: 100,
-          }}
-        />
+        <img src="/프로필 조회 화면.png" />
       </NavLink>
-    </>
+    </div>
   );
 }
